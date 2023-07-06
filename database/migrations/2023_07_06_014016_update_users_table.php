b3cb8b95d38193bf0e2d6dbb10cc23fc3336660d
Andres Romero <andres.romero.90@hotmail.com>
2023-07-05 22:50:22 -0300

    ⚡️ Added users table migrations

---
+++ database/migrations/2023_07_06_014016_update_users_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->enum('gender', ['male', 'female']);
+            $table->date('date_of_birth');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn(['first_name', 'last_name', 'gender', 'date_of_birth']);
+        });
+    }
+};
